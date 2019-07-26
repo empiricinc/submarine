e72ae91d45a76a3c0f55d97091c35d0c24c7fccb
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2019-07-26 13:12:17 +0500

    Tests Branch updated
    Summary
    -The header logo changed from bars to the HRMS in test-system.
    -The header logo changed from bars to the HRMS in training-files.

--- application/views/test-system/components/head.php
+++ application/views/test-system/components/head.php
@@ -40,7 +40,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span> 
         </button>
-        <a class="navbar-brand" href="#"><img src="<?php echo base_url('dashboardDesign/assets/img/bars.png'); ?>" alt=""></a>
+        <a class="navbar-brand" href="<?php echo base_url();?>"><div class="hrms-logo">HRMS</div></a>
       </div>
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
